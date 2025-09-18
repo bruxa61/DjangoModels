1bdbd04bea093dbfa03c14ae9645f35be5c9f6a6
Rafaela <rafaelaolbo@gmail.com>
2025-09-18 15:13:01 -0300

    view especialidade medico

--- core/medico/urls.py
+++ core/medico/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from . import views
+from .views import (EspecialidadeListView, EspecialidadeCreateView, EspecialidadeUpdateView,
+                    EspecialidadeDeleteView, MedicoListView, MedicoCreateView, MedicoUpdateView,
+                    MedicoDeleteView)
 
 urlpatterns = [
-    path('medicos/', views.lista_medicos, name='lista_medicos' ),
-    path('medicos/cadastrar/', views.cadastro_medico, name='cadastrar_medico' ),
-    path('medicos/editar/<int:id_medico>/', views.editar_medico, name='editar_medico' ),
-    path('medicos/deletar/<int:id_medico>/', views.deletar_medico, name='deletar_medico' ),
-    path('especialidades/', views.lista_especialidades, name='lista_especialidades' ),
-    path('especialidades/cadastrar/', views.cadastro_especialidade, name='cadastrar_especialidade' ),
-    path('especialidades/editar/<int:id_especialidade>/', views.editar_especialidade, name='editar_especialidade' ),
-    path('especialidades/deletar/<int:id_especialidade>/', views.deletar_especialidade, name='deletar_especialidade' ),
+    path('especialidades/', EspecialidadeListView.as_view(), name='especialidade_list'),
+    path('especialidades/create/', EspecialidadeCreateView.as_view(), name='especialidade_create'),
+    path('especialidades/<int:pk>/update/', EspecialidadeUpdateView.as_view(), name='especialidade_edit'),
+    path('especialidades/<int:pk>/delete/', EspecialidadeDeleteView.as_view(), name='especialidade_delete'),
+
+    path('medicos/', MedicoListView.as_view(), name='medico_list'),
+    path('medicos/create/', MedicoCreateView.as_view(), name='medico_create'),
+    path('medicos/<int:pk>/update/', MedicoUpdateView.as_view(), name='medico_edit'),
+    path('medicos/<int:pk>/delete/', MedicoDeleteView.as_view(), name='medico_delete'),
 ]
--- core/medico/views.py
+++ core/medico/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Medico, Especialidade
+from .forms import EspecialidadeForm, MedicoForm
 
 def lista_especialidades(request):
     contexto = {'especialidades': Especialidade.objects.all()}
@@ -74,3 +77,45 @@ def editar_medico(request, id_medico):
         'especialidades': Especialidade.objects.all() 
     }
     return render(request, 'medico/editar_medico.html', context=contexto)
+
+class EspecialidadeListView(ListView):
+    model = Especialidade
+    template_name = 'especialidade_list.html'
+
+class EspecialidadeCreateView(CreateView):
+    model = Especialidade
+    form_class = EspecialidadeForm
+    template_name = 'especialidade_form.html'
+    success_url = reverse_lazy('especialidade_list')
+
+class EspecialidadeUpdateView(UpdateView):
+    model = Especialidade
+    form_class = EspecialidadeForm
+    template_name = 'especialidade_form.html'
+    success_url = reverse_lazy('especialidade_list')
+
+class EspecialidadeDeleteView(DeleteView):
+    model = Especialidade
+    template_name = 'especialidade_confirm_delete.html'
+    success_url = reverse_lazy('especialidade_list')
+
+class MedicoListView(ListView):
+    model = Medico
+    template_name = 'medico_list.html'
+
+class MedicoCreateView(CreateView):
+    model = Medico
+    form_class = MedicoForm
+    template_name = 'medico_form.html'
+    success_url = reverse_lazy('medico_list')
+
+class MedicoUpdateView(UpdateView):
+    model = Medico
+    form_class = MedicoForm
+    template_name = 'medico_form.html'
+    success_url = reverse_lazy('medico_list')
+
+class MedicoDeleteView(DeleteView):
+    model = Medico
+    template_name = 'medico_confirm_delete.html'
+    success_url = reverse_lazy('medico_list')
